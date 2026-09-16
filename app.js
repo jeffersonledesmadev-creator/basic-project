4480be435ae52b1f5959062a7593f3b376f41ad6
Jefferson <jeffersonledesma.dev@gmail.com>
2026-09-16 00:02:04 -0500

    agregando listas

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var MainModule = (function(){
 
     var initApp = function(){
         console.log("app init");
+        configureEvents();
+    }
+    
+    var configureEvents = function(){
+        console.log("Configurando eventos");
     }
     
     return {
